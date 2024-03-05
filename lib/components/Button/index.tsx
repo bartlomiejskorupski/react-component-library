15b8695c68101471b70ba38c241357ad856924a4
Bartłomiej Skorupski <bartmann001@gmail.com>
2024-03-05 14:55:35 +0100

    Button init

--- lib/components/Button/index.tsx
+++ lib/components/Button/index.tsx
@@ -1,9 +1,20 @@
 import styles from './styles.module.css';
 
 type ButtonProps = {
-  label: string;
-};
+  className?: string;
+  rounder?: boolean;
+} & React.PropsWithChildren &
+  React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export default function Button(props: ButtonProps) {
-  return <button className={styles.button}>{props.label}</button>;
+  const { className, rounder, children, ...rest } = props;
+
+  const classes = `${className ?? ''} ${styles.button} ${
+    rounder ? styles.rounder : ''
+  }`;
+  return (
+    <button className={classes} {...rest}>
+      {children}
+    </button>
+  );
 }
