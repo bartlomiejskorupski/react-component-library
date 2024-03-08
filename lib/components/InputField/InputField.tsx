8223594fb4ac3ff7b5b34904c1f30f3c8f4b3f4f
Bartłomiej Skorupski <bartmann001@gmail.com>
2024-03-08 19:08:19 +0100

    InputField label

--- lib/components/InputField/InputField.tsx
+++ lib/components/InputField/InputField.tsx
@@ -1,24 +1,36 @@
-import { ForwardedRef, forwardRef, memo, useMemo } from 'react';
+import {
+  ForwardedRef,
+  InputHTMLAttributes,
+  forwardRef,
+  memo,
+  useMemo,
+} from 'react';
 import styles from './styles.module.css';
 
 type InputFieldProps = {
   className?: string;
+  label?: string;
   errorMessage?: string;
-};
+} & InputHTMLAttributes<HTMLInputElement>;
 
 const InputField = memo(
   forwardRef(function InputField(
     props: InputFieldProps,
     ref: ForwardedRef<HTMLInputElement>
   ) {
-    const { className, errorMessage } = props;
+    const { className, label, id, name, errorMessage, ...rest } = props;
 
     const classes = useMemo(() => {
       const classList = [className, styles.input, errorMessage && 'error'];
       return classList.filter((v) => v !== undefined).join(' ');
     }, [className, errorMessage]);
 
-    return <input ref={ref} className={classes} />;
+    return (
+      <div className={styles.container}>
+        <label htmlFor={id}>{label}</label>
+        <input id={id} name={name} ref={ref} className={classes} {...rest} />
+      </div>
+    );
   })
 );
 
--- src/components/InputFieldTest.tsx
+++ src/components/InputFieldTest.tsx
@@ -4,10 +4,10 @@ export default function InputFieldTest() {
   return (
     <section id="input-field-section">
       <h2>Input Field</h2>
-      <InputField />
-      <InputField />
-      <InputField />
-      <InputField />
+      <InputField label="First Name" id="fname" />
+      <InputField label="Last Name" id="lname" />
+      <InputField label="E-Mail" id="email" />
+      <InputField label="Password" id="password" />
     </section>
   );
 }
